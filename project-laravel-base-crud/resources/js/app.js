b0bb94bd64553ac71a1f7c6fcecaae18978a08e7
Davide <kalelc351k@gmail.com>
2021-02-01 15:54:35 +0100

    struttura principale creata

---
+++ project-laravel-base-crud/resources/js/app.js
@@ -0,0 +1,5 @@
+require('./bootstrap');
+ function init() {
+     console.log('Ciao Mondo!!!');
+ }
+ init();
---
+++ project-laravel-base-crud/resources/views/pages/home-page.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main-layout')
+@section('content')
+    
+    <main>
+        <h2>
+            MAIN
+        </h2>
+        <div>
+            
+        </div>
+    </main>
+
+@endsection
